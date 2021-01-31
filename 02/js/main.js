886fa4f136a8d82186640bd3bc502a570ab9cba8
Oksana Kharhenko <ksana.kharchenko@gmail.com>
2021-01-31 23:35:35 +0200

    innitial commit

--- 02/js/main.js
+++ 02/js/main.js
@@ -1,1 +1,41 @@
-//test commit
+var userAge = +prompt('Сколько лет?');
+
+var userSmokeStatus = prompt('Куришь?');
+
+/*if (userAge < 18 && (userSmokeStatus === 'Нет' || userSmokeStatus === 'нет')) {
+    alert('Так держать');
+}
+*/
+
+
+if (userAge < 18) {
+
+    switch (userSmokeStatus) {
+        case 'Нет':
+        case 'нет':
+            alert('Так держать!');
+            console.log('userAge->>', userAge);
+            break;
+        case 'Да':
+        case 'да':
+            alert('Маме расскажу');
+            console.log('userAge->>', userAge);
+            break;
+    }
+}
+
+if (userAge > 18) {
+
+    switch (userSmokeStatus) {
+        case 'Нет':
+        case 'нет':
+            alert('Молодец, и не надо');
+            console.log('useAge->>', userAge);
+            break;
+        case 'Да':
+        case 'да':
+            alert('Ну и зря');
+            console.log('userAge->>', userAge);
+            break;
+    }
+}
