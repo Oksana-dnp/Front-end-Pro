d23fdfbdb080d71c308cd0e31784ca953c24dad4
Oksana Kharhenko <ksana.kharchenko@gmail.com>
2021-02-07 22:05:44 +0200

    change

--- 04/js/main.js
+++ 04/js/main.js
@@ -1,6 +1,6 @@
 
-for (var a = 2; a <= 9; a++) {
-    for (var b = 2; b <= 9; b++) {
+for (a = 2; a <= 9; a++) {
+    for (b = 2; b <= 9; b++) {
         console.log(a + 'x' + b + '=' + a * b);
     }
     console.log('**********');
