c3633638bdaaeabf0b9a4a41f1bbf97d7c82ec49
Oksana Kharhenko <ksana.kharchenko@gmail.com>
2021-02-11 22:17:08 +0200

    task 5

--- 05/js/main.js
+++ 05/js/main.js
@@ -5,10 +5,12 @@ for (i = 1; i <= 3; i++) {
 }
 console.log(someArray);
 //Добавьте ему в начало элементы 4, 5, 6
+
+var someArr = [1, 2, 3];
 for (i = 6; i >= 4; i--) {
-    someArray.unshift(i)
+    someArr.unshift(i)
 }
-console.log(someArray);
+console.log(someArr);
 
 //Выведите на экран первый элемент
 var techSkillArray = ['js', 'css', 'jq'];
@@ -17,7 +19,7 @@ document.write(firstElement + `<br>`);
 
 //Выведите на экран последний элемент
 var lastElement = techSkillArray.pop();
-document.write(lastElement + `<br>`)
+document.write(lastElement + `<br>`);
 
 //С помощью метода slice запишите в новый элементы [1, 2, 3]
 var numberArray = [1, 2, 3, 4, 5];
