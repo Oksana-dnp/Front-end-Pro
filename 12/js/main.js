e4663faa8df2cd558a55df4b973697b6503bd4de
Oksana Kharhenko <ksana.kharchenko@gmail.com>
2021-03-10 22:16:21 +0200

    task_12

--- 12/js/main.js
+++ 12/js/main.js
@@ -1,21 +1,19 @@
+var mainDocument = document.querySelector("body");
+
 var feedbackBlock = document.createElement("div");
 var logo = document.createElement("img");
 
 var title = document.createElement("h1");
 title.textContent = "Создание сайта";
 
-//var titleText = document.createTextNode("Создание сайта");
-
-
-
 var subTitle = document.createElement("h2");
-subTitle.textContent = "Руководитель отдела маркетинга компании 'Stiker wall'"
+subTitle.textContent = "Руководитель отдела маркетинга компании 'Stiker wall'";
 
-var userComment = document.createElement("p");
-userComment.textContent = "Спасибо, команде за качественно проделанную работу.Сайт был создан в оговренные сроки и безединного бага. Рад, что друзья посоветовали именно Вас"
+var userComment = document.createElement("p")
+userComment.textContent = "Спасибо, команде за качественно проделанную работу. Сайт был создан в оговренные сроки и без единного бага.";
 
-
-var mainDocument = document.querySelector("body");
+var subUserComment = document.createElement('span');
+subUserComment.textContent = 'Рад, что друзья посоветовали именно Вас';
 
 function addElement(parentElem, addElem) {
     parentElem.appendChild(addElem)
@@ -26,7 +24,7 @@ addElement(feedbackBlock, logo);
 addElement(feedbackBlock, title);
 addElement(feedbackBlock, subTitle);
 addElement(feedbackBlock, userComment);
-
+addElement(userComment, subUserComment);
 
 function setAttr(elem, className) {
     elem.setAttribute("class", className)
@@ -37,6 +35,8 @@ setAttr(logo, "logo");
 setAttr(title, "title");
 setAttr(subTitle, "subtitle");
 setAttr(userComment, "user_text");
+setAttr(subUserComment, "user_subtext");
+
 
 
 
