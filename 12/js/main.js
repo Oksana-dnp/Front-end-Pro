9a21a23e7b2991ca91fb2f08946e17a7fd59b794
Oksana Kharhenko <ksana.kharchenko@gmail.com>
2021-03-10 22:03:01 +0200

    task_12

---
+++ 12/js/main.js
@@ -0,0 +1,47 @@
+var feedbackBlock = document.createElement("div");
+var logo = document.createElement("img");
+
+var title = document.createElement("h1");
+title.textContent = "Создание сайта";
+
+//var titleText = document.createTextNode("Создание сайта");
+
+
+
+var subTitle = document.createElement("h2");
+subTitle.textContent = "Руководитель отдела маркетинга компании 'Stiker wall'"
+
+var userComment = document.createElement("p");
+userComment.textContent = "Спасибо, команде за качественно проделанную работу.Сайт был создан в оговренные сроки и безединного бага. Рад, что друзья посоветовали именно Вас"
+
+
+var mainDocument = document.querySelector("body");
+
+function addElement(parentElem, addElem) {
+    parentElem.appendChild(addElem)
+}
+
+addElement(mainDocument, feedbackBlock);
+addElement(feedbackBlock, logo);
+addElement(feedbackBlock, title);
+addElement(feedbackBlock, subTitle);
+addElement(feedbackBlock, userComment);
+
+
+function setAttr(elem, className) {
+    elem.setAttribute("class", className)
+}
+
+setAttr(feedbackBlock, "feedback_container");
+setAttr(logo, "logo");
+setAttr(title, "title");
+setAttr(subTitle, "subtitle");
+setAttr(userComment, "user_text");
+
+
+
+
+
+
+
+
